746d3a2a796d5af5ec37f07ff7bf76149e437b7e
Jus' A Mere Dev <sandilemoremedi070501@gmail.com>
2022-02-06 04:04:53 +0200

    Finished the about page and footer

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,36 @@
+import Image from "next/image";
+
 export default function About() {
-  return <h1>About Us</h1>;
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <main>
+        <p>
+          Hi, I&apos;m Sandile Moremedi. I&apos;m a freelance website developer.
+          I had interests in cars since I was a young kid like most of the males
+          on earth. I haven&apos;t drove a car yet at the age of 20.
+          Disappointing.
+          <br />I always had this gut feeling that one day I&apos;m gonna own my
+          own Toyota Supra, Nissan 370z or Mitsubishi Evo, just to name some of
+          my dream cars.
+        </p>
+
+        <p>
+          Despite never driving a car, I was always and still am keen on knowing
+          everything that&apos;s inside a car. By inside I mean every piece of
+          of a car, from the steering wheel to the fly wheel, from the spring to
+          the piston, to the way the engine works.
+        </p>
+
+        <p>
+          So with all the skills I have, I decided to make a website where I can
+          share all the things I learn about a car, and also make it accessible
+          to everyone including you. You can write your own blog, you can check
+          out our car gallery and as time goes there will be more features for
+          everyone. So enjoy and feel home, this content is to be used by you to
+          your heart&apos;s desire.
+        </p>
+      </main>
+    </div>
+  );
 }
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact Us</h1>
+      <form>
+        <input type="text" name="name" id="name" placeholder="Enter Name" />
+        <input type="text" name="email" id="email" placeholder="Enter Email" />
+      </form>
+    </div>
+  );
+}
